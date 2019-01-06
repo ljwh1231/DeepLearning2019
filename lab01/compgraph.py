d4453bee38b21ad68267eb70470698fc9aa40cc0
이정현 <jhlee@ijeonghyeon-ui-MacBook-Pro.local>
2019-01-06 22:33:36 +0900

    ?

--- lab01/compgraph.py
+++ lab01/compgraph.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+# build graph using TF operations
 node1 = tf.constant(3.0, tf.float32)
 node2 = tf.constant(4.0)
 node3 = tf.add(node1, node2) # node3 = node1 + node2
@@ -9,6 +9,7 @@ print("node3 : ", node3)
 
 # node들을 모두 tensor라는 datatype이므로 원하는 결과가 나오지는 않음
 
+#Feed data and run graph
 sess = tf.Session()
 print("sess.run(node1, node2) :", sess.run([node1, node2]))
 print("sess.run(node3) : ", sess.run(node3))
